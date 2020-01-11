2951d8470785d1b14143ce595a129764c9419ce7
davidmcq137 <davidmcq@us.ibm.com>
2020-01-11 15:17:47 -0500

    Initial Commit

---
+++ BLETest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BLETest
+//
+//  Created by David Mcqueeney on 1/11/20.
+//  Copyright © 2020 David Mcqueeney. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
